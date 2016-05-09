3d96552d7743581c56b5dbe57f18bdce933d1c95
shash91 <shashika@davton.com>
2016-05-09 11:52:30 +0530

    The concept of the Controller Factory
    Creating a Custom Controller Factory to understand how controller
    factories are working

---
+++ ControllerExtensibility/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ControllerExtensibility.Models;
+
+namespace ControllerExtensibility.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View("Result",new Result { ControllerName="Customer",ActionName="Index"});
+        }
+        public ViewResult List()
+        {
+            return View("Result", new Result { ControllerName = "Customer", ActionName = "List" });
+        }
+    }
+}
---
+++ ControllerExtensibility/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using ControllerExtensibility.Infrastructure;
+
+namespace ControllerExtensibility
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            ControllerBuilder.Current.SetControllerFactory(new CustomControllerFactory());
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
